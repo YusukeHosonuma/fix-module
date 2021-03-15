af0b1b03d041c8802602174bc1436d70b4f1716d
Yusuke Hosonuma <tobi462@gmail.com>
2021-03-15 19:08:02 +0900

    add: catch exception

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import           Control.Exception
 import           Control.Monad
 import           FixModule
 import           System.Directory
 import           System.Environment
-import           System.Exit
+import           System.Exit        (exitFailure, exitSuccess)
 
 main :: IO ()
 main = do
@@ -13,14 +16,21 @@ main = do
     exist <- doesFileExist "package.yaml"
     unless exist exitPackageYamlNotFound
     pwd <- getCurrentDirectory
-    fixModule pwd  -- TODO: 例外処理
+    fixModule pwd `catch` reportException
 
 printVersion :: IO ()
 printVersion = do
-    putStrLn "fix-module 0.1.0.0"
+    putStrLn "fix-module 0.1.0"
     exitSuccess
 
 exitPackageYamlNotFound :: IO ()
 exitPackageYamlNotFound = do
-    putStrLn "package.yaml is not found. (not support .cabal yet)"
+    putStrLn "A package.yaml is not found. (Not support .cabal yet)"
     exitFailure
+
+reportException :: SomeException -> IO ()
+reportException e = do
+    putStrLn "Fatal error.\n"
+    putStrLn $ "[Cause]\n" ++ displayException e ++ "\n"
+    putStrLn "Please report issue when reproducible."
+    putStrLn "https://github.com/YusukeHosonuma/fix-module/issues/new"
